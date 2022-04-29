0953832a2006c23541bcaa99056344685e4dac03
Dmytro Krasovsky <kdi@opus.nett.npolar>
2022-04-29 09:43:18 +0200

    customize fish prompt

--- config.fish
+++ config.fish
@@ -11,6 +11,26 @@ function ll
     ls -lh $argv
 end
 
+function zos_prompt_jobs
+  set -l num_jobs (jobs | wc -l | xargs)
+
+  if test $num_jobs -ne 0
+    printf "%s[%s]%s" (set_color yellow) $num_jobs (set_color normal)
+  end
+end
+
+function zos_prompt_user
+  printf "%s%s@%s%s%s" (set_color green) $USER $hostname (set_color normal)
+end
+
+function zos_prompt_pwd
+  printf "%s%s%s" (set_color blue) (prompt_pwd) (set_color normal)
+end
+
+function fish_prompt
+  printf '%s:%s%s%s$ ' (zos_prompt_user) (zos_prompt_pwd) (zos_prompt_jobs) (fish_git_prompt)
+end
+
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showcolorhints 'yes'
