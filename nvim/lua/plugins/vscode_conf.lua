8ed0762133b90770623597a02410fe96610681d8
Dmytro Krasovsky <kdi@opus.nett.npolar>
2022-08-05 11:04:59 +0200

    etc

--- nvim/lua/plugins/vscode_conf.lua
+++ nvim/lua/plugins/vscode_conf.lua
@@ -2,6 +2,8 @@ vim.cmd("colorscheme vscode")
 
 local c = require('vscode.colors')
 
+-- vim.highlight.create('EndOfBuffer', { guibg = 0, guifg = c.vscLineNumber })
+
 require("vscode").setup({
   transparent = true,
   disable_nvimtree_bg = true,
@@ -12,7 +14,7 @@ require("vscode").setup({
     EndOfBuffer = { fg = c.vscLineNumber },
     StatusLine = { fg = "#5b5b5b", bg = c.vscNone },
     StatusLineNC = { fg = c.vscLineNumber },
-    StatusLineFileName = { fg = c.vscYellowOrange },
+    StatusLineFileName = { fg = c.vscFront },
     StatusLineModified = { fg = c.vscYellowOrange },
     -- lsp diagnostics
     LspDiagnosticsDefaultError = { fg = c.vscLightRed },
@@ -35,38 +37,3 @@ require("vscode").setup({
     TabLineSel = { fg = c.vscYellowOrange },
   }
 })
-
-
--- vim.highlight.create('EndOfBuffer', { guibg = 0, guifg = c.vscLineNumber })
--- vim.highlight.create('StatusLine', { guibg = 0, guifg = c.vscLineNumber })
--- vim.highlight.create('StatusLineNC', { guibg = 0, guifg = c.vscLineNumber })
--- vim.highlight.create('StatusLineFileName', { guibg = 0, guifg = c.vscYellowOrange })
--- vim.highlight.create('StatusLineModified', { guibg = 0, guifg = c.vscYellowOrange })
-
--- vim.highlight.create('TabLine', { guibg = 0, guifg = c.vscLineNumber })
--- vim.highlight.create('TabLineFill', { guibg = 0, guifg = c.vscLineNumber })
--- vim.highlight.create('TabLineSel', { guibg = 0, guifg = c.vscYellowOrange })
-
--- vim.highlight.create('LspDiagnosticsDefaultError', { guibg = 0, guifg = c.vscLightRed })
--- vim.highlight.create('LspDiagnosticsSignError', { guibg = 0, guifg = c.vscLightRed })
--- vim.highlight.create('LspDiagnosticsDefaultWarning', { guibg = 0, guifg = c.vscYellow })
--- vim.highlight.create('LspDiagnosticsSignWarning', { guibg = 0, guifg = c.vscYellow })
-
--- vim.highlight.create('SignColumn', { guibg = 0 })
--- vim.highlight.create('StatusLine', { guibg = 0 })
--- vim.highlight.create('CursorLineNr', { guibg = 0 })
---
--- vim.highlight.create('VertSplit', { guifg = c.vscLightRed })
--- vim.highlight.create('SignatureMarkText', { guibg = 0, guifg = c.vscPink })
-
--- vim.highlight.create('NvimTreeRootFolder', { guibg = 0, guifg = c.vscGray })
--- vim.highlight.create('NvimTreeFolderName', { guibg = 0, guifg = c.vscBlue })
--- vim.highlight.create('NvimTreeOpenedFolderName', { guibg = 0, guifg = c.vscLightBlue })
--- vim.highlight.create('NvimTreeEmptyFolderName', { guibg = 0, guifg = c.vscLightBlue })
--- vim.highlight.create('NvimTreeSpecialFile', { guibg = 0, guifg = c.vscLightGreen })
--- vim.highlight.create('NvimTreeExecFile', { guibg = 0, guifg = c.vscLightRed })
--- vim.highlight.create('NvimTreeSymlink', { guibg = 0, guifg = c.vscOrange })
--- vim.highlight.create('NvimTreeOpenedFile', { guibg = 0, guifg = c.vscPink })
--- vim.highlight.create('NvimTreeImageFile', { guibg = 0, guifg = c.vscYellow })
-
--- vim.highlight.create('NvimTreeIndentMarker', { guibg = 0, guifg = c.vscYellow })
