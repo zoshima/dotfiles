014cc4e9a5d32a8032f1e625bcd6666b198c7b01
zoshima <dmykras@gmail.com>
2023-02-22 10:26:29 +0100

    minimal links

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 rm  ~/.vimrc || true
-rm  ~/.tmux.conf || true
-rm  ~/.alacritty.yml || true
 rm  ~/.gitconfig || true
+rm  ~/.config/fish/config.fish || true
 
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
-ln -s ~/git/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/git/dotfiles/.alacritty.yml ~/.alacritty.yml
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/git/dotfiles/config.fish ~/.config/fish/config.fish
