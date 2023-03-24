daabfafd68a6589e1212dd2ca134d2ed2052d990
zoshima <dmykras@gmail.com>
2023-03-24 09:59:20 +0100

    switch to bash

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -2,8 +2,7 @@
 
 rm  ~/.vimrc || true
 rm  ~/.gitconfig || true
-rm  ~/.config/fish/config.fish || true
 
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/git/dotfiles/config.fish ~/.config/fish/config.fish
+ln -s ~/git/dotfiles/.bashrc ~/.bashrc
