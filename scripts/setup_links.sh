3132aa8c9a2d07de0e3256ad605d5e629c50d469
zoshima <dmykras@gmail.com>
2023-06-14 11:14:22 +0200

    remove vscode links

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -7,9 +7,3 @@ rm  ~/.bashrc || true
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
-
-rm ~/.config/Code\ -\ OSS/User/keybindings.json
-rm ~/.config/Code\ -\ OSS/User/settings.json
-
-ln -s ~/git/dotfiles/vscode/keybindings.json ~/.config/Code\ -\ OSS/User/
-ln -s ~/git/dotfiles/vscode/settings.json ~/.config/Code\ -\ OSS/User/
