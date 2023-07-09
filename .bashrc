6dd881b3c1a38f08a6975508197b721b8804b426
zoshima <dmykras@gmail.com>
2023-07-09 11:29:50 +0200

    use env var for all colors

--- .bashrc
+++ .bashrc
@@ -1,27 +1,53 @@
+export COL_BG="#1C1C1C"
+
+export COL_WHITE="#ffffff"
+export COL_GRAY="#616161"
+
+export COL_RED="#e57373"
+export COL_GREEN="#aed581"
+export COL_BLUE="#4fc3f7"
+
+export COL_PINK="#f06292"
+export COL_YELLOW="#fff176"
+export COL_ORANGE="#ffb74d"
+
+fromhex() {
+  hex=$1
+
+  if [[ $hex == "#"* ]]; then
+    hex=$(echo $1 | awk '{print substr($0,2)}')
+  fi
+
+  r=$(printf '0x%0.2s' "$hex")
+  g=$(printf '0x%0.2s' ${hex#??})
+  b=$(printf '0x%0.2s' ${hex#????})
+
+  echo -e `printf "%d" "$(((r<75?0:(r-35)/40)*6*6+(g<75?0:(g-35)/40)*6+(b<75?0:(b-35)/40)+16))"`
+}
+
 
 set_prompt() {
 	F_RESET="$(tput sgr0)"
 	F_BOLD="$(tput bold)"
-	F_CYAN="$(tput setaf 80)"
-	F_BLUE="$(tput setaf 75)"
-	F_LIGHT_BLUE="$(tput setaf 81)"
-  F_YELLOW="$(tput setaf 228)"
-  F_GREEN="$(tput setaf 114)"
-  F_ORANGE="$(tput setaf 215)"
+  F_BLUE="$(tput setaf $(fromhex $COL_BLUE))"
+  F_YELLOW="$(tput setaf $(fromhex $COL_YELLOW))"
+  F_GREEN="$(tput setaf $(fromhex $COL_GREEN))"
+  F_ORANGE="$(tput setaf $(fromhex $COL_ORANGE))"
+  F_RED="$(tput setaf $(fromhex $COL_RED))"
 
   git_status() {
     if [ $(git branch 2>/dev/null | wc -l) -gt 0 ]; then
       git_branch="$(git rev-parse --abbrev-ref HEAD)"
 
       if [ $(git status --porcelain | wc -l) -gt 0 ]; then
-        git_branch="$git_branch$F_ORANGE*$F_RESET"
+        git_branch="$git_branch$F_RED*$F_RESET"
       fi
 
       echo "($git_branch)"
     fi
   }
 
-	user="$F_BOLD$F_LIGHT_BLUE\u$F_RESET"
+	user="$F_GREEN\u$F_RESET"
 	host="\h"
 	directory="$F_RESET$F_BOLD$F_BLUE\w$F_RESET"
 	num_jobs="$F_ORANGE\$([ \j -gt 0 ] && echo [\j])$F_RESET"
--- nvim/lua/colorscheme/init.lua
+++ nvim/lua/colorscheme/init.lua
@@ -1,100 +1,78 @@
 local c = require('colorscheme.colors')
-c["None"] = "NONE"
-c["Black"] = "#000000"
-c["White"] = "#FFFFFF"
-c["Gray"] = "#616161"
-c["GnomeGray"] = "#2e2e2e"
 
 local conf = {
   None = {
-    { Name = "Pmenu", Value = { bg = c.GnomeGray } },
+    { Name = "Pmenu",      Value = { bg = c.Gray } },
     { Name = "PmenuThumb", Value = { bg = c.White } },
-    { Name = "PmenuSbar", Value = { bg = c.Gray } },
-    { Name = "PmenuSel", Value = { reverse = true } },
-    { Name = "Visual", Value = { bg = c.Gray } },
-    { Name = "VisualNOS", Value = { bg = c.Gray } },
+    { Name = "PmenuSbar",  Value = { bg = c.Gray } },
+    { Name = "PmenuSel",   Value = { reverse = true } },
+    { Name = "Visual",     Value = { bg = c.Gray } },
+    { Name = "VisualNOS",  Value = { bg = c.Gray } },
   },
+  White = {
+    -- UI
+    { Name = "Normal" },
+    { Name = "CursorLineNr" },
+    { Name = "StatusLineFileName" },
 
-  Black = {
+    -- syntax
+    { Name = "Operator" },   -- >, = etc
+    { Name = "Identifier" }, -- [Identifier, Function]
+    { Name = "Constant" },   -- [String, Character, Number, Boolean, Float]
+    { Name = "Delimiter" },
   },
   Gray = {
     { Name = "DiagnosticHint" },
-
     { Name = "TelescopeBorder" },
 
     -- UI
-    { Name = "StatusLine", Value = { nocombine = true } },
+    { Name = "StatusLine",     Value = { nocombine = true } },
     { Name = "StatusLineNC" },
-    { Name = "LineNr"},
+    { Name = "LineNr" },
     { Name = "NonText" },
     { Name = "VertSplit" },
 
     -- syntax
     { Name = "Ignore" }, -- blank, hidden, ignored
-  },
-  White = {
-    -- UI
-    { Name = "Normal"},
-    { Name = "CursorLineNr" },
-    { Name = "StatusLineFileName" },
-
-    -- syntax
-    { Name = "Operator" }, -- >, = etc
-    { Name = "Identifier" }, -- [Identifier, Function]
-    { Name = "Constant" }, -- [String, Character, Number, Boolean, Float]
-    { Name = "Delimiter" },
+    { Name = "Comment" },
+    { Name = "Folded",         Value = {} },
   },
 
   Red = {
     { Name = "NvimTreeExecFile" },
     { Name = "DiagnosticError" },
 
-     -- UI
-    { Name = "ErrorMsg"}, -- err in output
+    -- UI
+    { Name = "ErrorMsg" }, -- err in output
 
     -- syntax
     { Name = "Error" }, -- any erronous construct
   },
-  Pink = {
-  },
-  Purple = {
-  },
+  Green = {
+    { Name = "NvimTreeOpenedFile" },
 
-  DeepPurple = {
-  },
-  Indigo = {
+    { Name = "Title" }, -- titles in MD etc
+    { Name = "Type" },  -- [StorageClass, Structure, Typedef]
   },
   Blue = {
     { Name = "DiagnosticInfo" },
-  },
-  LightBlue = {
+
     { Name = "NvimTreeSymlink" },
     { Name = "NvimTreeFolderName" },
     { Name = "NvimTreeOpenedFolderName" },
     { Name = "NvimTreeEmptyFolderName" },
 
     -- syntax
-    { Name = "PreProc" }, -- [Include, Define, Macro, PreCondit]
+    { Name = "PreProc" },   -- [Include, Define, Macro, PreCondit]
     { Name = "Statement" }, -- [Conditional, Repeat, Label, Operator, Keyword, Exception]
-    { Name = "Special" }, -- [SpecialChar, Tag, Delimiter, SpecialComment, Debug]
+    { Name = "Special" },   -- [SpecialChar, Tag, Delimiter, SpecialComment, Debug]
   },
-  Cyan = {
-  },
-  Teal = {
-    -- syntax
-    { Name = "Todo" }, -- extra attention
-    { Name = "Underlined" }, -- outstanding text, html links
-    { Name = "Search", Value = { standout = true } },
-  },
-  Green = {
-  },
-  LightGreen = {
-    { Name = "NvimTreeOpenedFile" },
 
-    { Name = "Title" }, -- titles in MD etc
-    { Name = "Type" }, -- [StorageClass, Structure, Typedef]
-  },
-  Lime = {
+  Pink = {
+    -- syntax
+    { Name = "Todo" },       -- extra attention
+    { Name = "Underlined" }, -- outstanding text, html links
+    { Name = "Search",    Value = { standout = true } },
   },
   Yellow = {
     { Name = "NvimTreeSpecialFile" },
@@ -107,8 +85,6 @@ local conf = {
     -- syntax
     { Name = "Function" }, -- function names
   },
-  Amber = {
-  },
   Orange = {
     { Name = "NvimTreeImageFile" },
 
@@ -120,21 +96,15 @@ local conf = {
     { Name = "Float" },
     { Name = "Special" }, -- any special symbol
   },
-  DeepOrange = {
-  },
-  Brown = {
-    { Name = "Folded", Value = {} },
-  },
-  BlueGray = {
-    { Name = "Comment" },
-  },
 }
 
 for col_name, values in pairs(conf) do
-  for i, v in ipairs(values) do
+  for _, v in ipairs(values) do
+    local value
+
     if v.Value then
       value = v.Value
-    else 
+    else
       value = { bg = c.None }
     end
 
@@ -145,4 +115,3 @@ for col_name, values in pairs(conf) do
 end
 
 vim.api.nvim_set_hl(0, "CursorLine", { bg = c.None })
-
