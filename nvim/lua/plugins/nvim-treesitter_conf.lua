05bea084616d6bec6f036487edce0cbd67bcd4ee
zoshima <dmykras@gmail.com>
2023-08-17 15:00:16 +0200

    set treesitter languages

--- nvim/lua/plugins/nvim-treesitter_conf.lua
+++ nvim/lua/plugins/nvim-treesitter_conf.lua
@@ -1,6 +1,19 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "all",
-  ignore_install = {"t32"},
+  modules = {},
+  sync_install = true,
+  auto_install = false,
+  ensure_installed = {
+    "lua",
+    "json",
+    "javascript",
+    "typescript",
+    "html",
+    "css",
+    "scss",
+    "go",
+    "python",
+  },
+  ignore_install = { "t32" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
