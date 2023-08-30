94aada4bd14464d534954cd64beee1e54d5aef82
Dmytro Krasovsky <dmykras@gmail.com>
2023-08-30 17:03:12 +0200

    add sway conf

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -3,7 +3,13 @@
 rm  ~/.vimrc || true
 rm  ~/.gitconfig || true
 rm  ~/.bashrc || true
+rm  ~/.alacritty.yml || true
+rm -rf ~/.config/sway || true
+
+mkdir -p ~/.config/sway
 
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
+ln -s ~/git/dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -s ~/git/dotfiles/.sway.conf ~/.config/sway/config
