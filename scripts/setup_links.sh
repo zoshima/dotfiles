ff7e5075d3e3ae328f188765b3bf14123420d77b
zoshima <dmykras@gmail.com>
2023-09-14 10:53:31 +0200

    replace sway with tmux conf

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -4,12 +4,10 @@ rm  ~/.vimrc || true
 rm  ~/.gitconfig || true
 rm  ~/.bashrc || true
 rm  ~/.alacritty.yml || true
-rm -rf ~/.config/sway || true
-
-mkdir -p ~/.config/sway
+rm  ~/.tmux.conf || true
 
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
 ln -s ~/git/dotfiles/.alacritty.yml ~/.alacritty.yml
-ln -s ~/git/dotfiles/.sway.conf ~/.config/sway/config
+ln -s ~/git/dotfiles/.tmux.conf ~/.tmux.conf
