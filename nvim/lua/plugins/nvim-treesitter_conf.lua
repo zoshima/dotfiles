a16757fbdedc9702cf1d6cb7dc9b95f396b378ae
zoshima <dmykras@gmail.com>
2024-01-23 10:08:16 +0100

    disable context

--- nvim/lua/plugins/nvim-treesitter_conf.lua
+++ nvim/lua/plugins/nvim-treesitter_conf.lua
@@ -23,11 +23,4 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-});
-
-require("treesitter-context").setup({
-  enable = true,
-  max_lines = 1,
-  trim_scope = "inner",
-  mode = "topline",
-});
+})
