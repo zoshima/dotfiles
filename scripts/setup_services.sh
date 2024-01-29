e19a78d3f3be40eae736c127eb9729c9051250e3
zoshima <dmykras@gmail.com>
2024-01-29 23:11:01 +0100

    copy service and script instead of symlink

--- scripts/setup_services.sh
+++ scripts/setup_services.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-sudo ln -s /home/kdi/git/dotfiles/services/auraoff.service /etc/systemd/system/auraoff.service
-sudo systemctl enable auraoff.service
+
+if [[ $UID != 0 ]]; then
+    echo "try: sudo $0 $*"
+    exit 1
+fi
+
+systemctl stop auraoff.service
+systemctl disable auraoff.service
+
+rm /etc/systemd/system/auraoff.service
+rm /etc/systemd/system/multi-user.target.wants/auraoff.service
+rm /bin/auraoff.sh
+
+cp /home/kdi/git/dotfiles/services/auraoff.service /etc/systemd/system/auraoff.service
+cp /home/kdi/git/dotfiles/scripts/auraoff.sh /bin/auraoff.sh
+
+systemctl enable auraoff.service
+systemctl start auraoff.service
