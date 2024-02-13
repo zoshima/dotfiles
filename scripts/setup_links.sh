77e56fe8b6fe11639d72bdb55e593be03fd78eac
zoshima <dmykras@gmail.com>
2024-02-13 15:12:11 +0100

    add checkupdates script

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+mkdir -p ~/.local/bin/
+
 rm  ~/.vimrc || true
 rm  ~/.gitconfig || true
 rm  ~/.bashrc || true
+rm ~/.local/bin/checkupdates.sh || true
 
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
+ln -s ~/git/dotfiles/scripts/checkupdates.sh ~/.local/bin/checkupdates.sh
