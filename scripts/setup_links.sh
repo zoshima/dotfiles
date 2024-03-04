bc019d11d0bc6185321cb4d8c845032111ffe8d6
zoshima <dmykras@gmail.com>
2024-03-04 23:09:58 +0100

    add alacritty config

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -5,9 +5,11 @@ mkdir -p ~/.local/bin/
 rm  ~/.vimrc || true
 rm  ~/.gitconfig || true
 rm  ~/.bashrc || true
+rm  ~/.alacritty.toml || true
 rm ~/.local/bin/checkupdates.sh || true
 
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
+ln -s ~/git/dotfiles/.alacritty.toml  ~/.alacritty.toml
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
 ln -s ~/git/dotfiles/scripts/checkupdates.sh ~/.local/bin/checkupdates.sh
