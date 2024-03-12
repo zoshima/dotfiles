9785a86dd873c5a953b0ecfcb76fce826d282f48
zoshima <dmykras@gmail.com>
2024-03-12 09:02:54 +0100

    readd tmux

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -6,10 +6,12 @@ rm  ~/.vimrc || true
 rm  ~/.gitconfig || true
 rm  ~/.bashrc || true
 rm  ~/.alacritty.toml || true
+rm  ~/.tmux.conf || true
 rm ~/.local/bin/checkupdates.sh || true
 
 ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
 ln -s ~/git/dotfiles/.alacritty.toml  ~/.alacritty.toml
+ln -s ~/git/dotfiles/.tmux.conf  ~/.tmux.conf
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
 ln -s ~/git/dotfiles/scripts/checkupdates.sh ~/.local/bin/checkupdates.sh
