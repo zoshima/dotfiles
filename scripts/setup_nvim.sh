099daf2bb9c9bb5a3dc4bf700917ed262dfe1b59
zoshima <dmykras@gmail.com>
2024-06-16 09:51:24 +0200

    use submodules for plugins

--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -5,12 +5,6 @@ mkdir -p ~/.config/nvim
 
 ln -s ~/git/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 ln -s ~/git/dotfiles/nvim/lua ~/.config/nvim/lua
+ln -s ~/git/dotfiles/nvim/pack ~/.config/nvim/pack
 
 nvim ~/.config/nvim/lua/plugins/init.lua
-
-# plugins
-
-git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig
-git clone https://github.com/nvim-treesitter/nvim-treesitter.git ~/.config/nvim/pack/nvim/start/nvim-treesitter
-git clone https://github.com/nvim-lua/plenary.nvim.git ~/.config/nvim/pack/nvim/start/plenary
-git clone https://github.com/nvim-telescope/telescope.nvim.git ~/.config/nvim/pack/nvim/start/telescope
