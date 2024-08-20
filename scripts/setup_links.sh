147c1927dd8d08d5c1e774c1909ce2ea63d8bf31
zoshima <dmykras@gmail.com>
2024-08-20 08:27:18 +0200

    comment out unused dotfiles

--- scripts/setup_links.sh
+++ scripts/setup_links.sh
@@ -2,16 +2,16 @@
 
 mkdir -p ~/.local/bin/
 
-rm  ~/.vimrc || true
-rm  ~/.gitconfig || true
-rm  ~/.bashrc || true
-rm  ~/.alacritty.toml || true
-rm  ~/.tmux.conf || true
-rm ~/.local/bin/checkupdates.sh || true
+# rm ~/.vimrc || true
+rm ~/.gitconfig || true
+rm ~/.bashrc || true
+# rm  ~/.alacritty.toml || true
+# rm  ~/.tmux.conf || true
+# rm ~/.local/bin/checkupdates.sh || true
 
-ln -s ~/git/dotfiles/.vimrc  ~/.vimrc
-ln -s ~/git/dotfiles/.alacritty.toml  ~/.alacritty.toml
-ln -s ~/git/dotfiles/.tmux.conf  ~/.tmux.conf
+# ln -s ~/git/dotfiles/.vimrc ~/.vimrc
+# ln -s ~/git/dotfiles/.alacritty.toml  ~/.alacritty.toml
+# ln -s ~/git/dotfiles/.tmux.conf  ~/.tmux.conf
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
-ln -s ~/git/dotfiles/scripts/checkupdates.sh ~/.local/bin/checkupdates.sh
+# ln -s ~/git/dotfiles/scripts/checkupdates.sh ~/.local/bin/checkupdates.sh
