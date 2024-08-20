3a865e40c8547907d0746515155e2f00033deadb
dkra <dmykras@gmail.com>
2024-08-20 08:26:13 +0200

    add GitBlame fn

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,10 +40,35 @@ function MapKey(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+function GitBlame()
+  local current_win = vim.api.nvim_get_current_win()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local current_row, _ = unpack(vim.api.nvim_win_get_cursor(current_win))
+
+  vim.cmd("vertical new")
+
+  local new_win = vim.api.nvim_get_current_win()
+  local new_buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_set_option_value("readonly", true, { buf = new_buf })
+  vim.api.nvim_set_option_value("buftype", "nofile", { buf = new_buf })
+  vim.api.nvim_set_option_value("bufhidden", "delete", { buf = new_buf })
+
+  local blame_output = vim.fn.systemlist("git blame " .. vim.api.nvim_buf_get_name(current_buf))
+  vim.api.nvim_buf_set_lines(new_buf, 0, -1, false, blame_output)
+
+  vim.api.nvim_win_set_cursor(new_win, { current_row, 0 })
+end
+
 function ShowHighlightInfo()
   local result = vim.treesitter.get_captures_at_cursor(0)
   print(vim.inspect(result))
 end
+
+-- commands
+vim.cmd([[
+  command! GitBlame lua GitBlame()
+]])
 
 -- mappings
 MapKey("n", "*", "*``")
@@ -58,6 +83,7 @@ MapKey("n", "<C-Up>", ":resize +5<CR>")
 MapKey("n", "<C-Down>", ":resize -5<CR>")
 MapKey("n", "<Space>,", ":noh<CR>")
 MapKey("n", "g?", ":lua ShowHighlightInfo()<CR>")
+MapKey("n", "gb", ":lua GitBlame()<CR>")
 MapKey("n", "<F5>", ":!make run<CR>")
 MapKey("n", "<F6>", ":!make build<CR>")
 MapKey("n", "<F7>", ":!make test<CR>")
