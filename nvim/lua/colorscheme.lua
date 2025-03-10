47f2866e4d10f962c141945ab70641cd58901da2
zoshima <dmykras@gmail.com>
2025-03-10 10:56:35 +0100

    simplify colorscheme

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,177 +1,57 @@
--- :h group-name
 -- :h cterm-colors
+-- :h group-name
 
-local syntax_colors = {
-  White = {
-    "Identifier",
-  },
-  Gray = {
-    "Delimiter", -- Special group
-    "Operator",  -- Statement group
-    "Comment",
-    "NonText",   -- ~ at EOF
-    "PreProc",
-  },
-  Green = {
-    "Constant",
-    "String",
-    "Added",
-  },
-  Yellow = {
-    "Function", -- Identifier group
-    "Changed",
-  },
-  Brown = {
-    "Todo",
-  },
-  Magenta = {
-    "Special",
-  },
-  Blue = {
-    "Tag", -- Special group
-  },
-  Cyan = {
-    "Type",
-  },
-  Red = {
-    "Statement",
-    "Error",
-    "Removed",
-  },
-};
+-- syntax (builtin)
+vim.api.nvim_set_hl(0, "Comment", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "Constant", { ctermfg = "Green", });
+vim.api.nvim_set_hl(0, "Identifier", { ctermfg = "White", });
+vim.api.nvim_set_hl(0, "Function", { ctermfg = "Yellow", });
+vim.api.nvim_set_hl(0, "Statement", { ctermfg = "Red", });
+vim.api.nvim_set_hl(0, "Operator", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "PreProc", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "Type", { ctermfg = "Cyan", });
+vim.api.nvim_set_hl(0, "Special", { ctermfg = "Magenta", });
+vim.api.nvim_set_hl(0, "Tag", { ctermfg = "Blue", });
+vim.api.nvim_set_hl(0, "Delimiter", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "Underlined", { underline = true });
+vim.api.nvim_set_hl(0, "Ignore", { ctermfg = "DarkGray", });
+vim.api.nvim_set_hl(0, "Error", { ctermfg = "Red", });
+vim.api.nvim_set_hl(0, "Todo", { ctermfg = "Brown", });
+vim.api.nvim_set_hl(0, "Added", { ctermfg = "Green", });
+vim.api.nvim_set_hl(0, "Changed", { ctermfg = "Yellow", });
+vim.api.nvim_set_hl(0, "Removed", { ctermfg = "Red", });
 
-local ui_colors = {
-  Normal = { ctermbg = "NONE", bg = "Black" },
-  Pmenu = { ctermbg = "Black" },
-  PmenuSbar = { ctermbg = "Black", },
-  PmenuThumb = { ctermbg = "Black", reverse = true, },
-  PmenuSel = { ctermbg = "White", ctermfg = "Black", },
-  NormalFloat = { ctermbg = "NONE", },
-  FloatBorder = { ctermfg = "DarkGray", ctermbg = "NONE" },
-  SignColumn = { ctermbg = "NONE", },
-  Visual = { ctermbg = "NONE", reverse = true },
-  TabLineFill = { ctermfg = "DarkGray", },
-  TabLineSel = { ctermfg = "White", },
-  StatusLine = { ctermfg = "DarkGray" },
-  StatusLineNC = { ctermfg = "DarkGray", nocombine = true },
-  StatusLineFileName = { ctermfg = "White" },
-  CursorLineNr = { ctermfg = "White" },
-  WinSeparator = { ctermfg = "DarkGray" },
-  MatchParen = { ctermbg = "DarkGray" },
-  LineNr = { ctermfg = "DarkGray" },
-  MsgSeparator = { ctermfg = "DarkGray" },
-  Underlined = { underline = true },
-  Search = { ctermbg = "Yellow", ctermfg = "Black" },
-  CurSearch = { ctermbg = "Yellow", ctermfg = "Black" },
-  IncSearch = { reverse = true },
-  DiagnosticError = { ctermfg = "Red" },
-  DiagnosticUnderlineError = { sp = "Red", undercurl = true },
-  DiagnosticWarn = { ctermfg = "Yellow" },
-  DiagnosticUnderlineWarn = { sp = "Yellow", undercurl = true },
-  DiagnosticInfo = { ctermfg = "Blue" },
-  DiagnosticUnderlineInfo = { sp = "Blue", undercurl = true },
-  DiagnosticHint = { ctermfg = "Cyan" },
-  DiagnosticUnderlineHint = { sp = "Cyan", undercurl = true },
-  DiagnosticOk = { ctermfg = "Green" },
-  DiagnosticUnderlineOk = { sp = "Green", undercurl = true },
-  Directory = { ctermfg = "Blue", bold = true },
-};
+-- syntax (treesitter)
+vim.api.nvim_set_hl(0, "@constructor", { link = "Function" });
+vim.api.nvim_set_hl(0, "@type.builtin", { ctermfg = "Blue", italic = true });
+vim.api.nvim_set_hl(0, "@constant.builtin", { ctermfg = "Green", italic = true });
+vim.api.nvim_set_hl(0, "@function.builtin", { ctermfg = "Yellow", italic = true });
+vim.api.nvim_set_hl(0, "@variable.builtin", { ctermfg = "White", italic = true });
+vim.api.nvim_set_hl(0, "@module", { link = "PreProc" });
 
-local group_links = {
-  Error = {
-    "ErrorMsg",
-  },
-  Warning = {
-    "WarningMsg",
-  },
-  Statement = {
-    "@type.qualifier",
-    "@storageclass",
-  },
-  Type = {
-    "@tag.attribute",
-    "@type.builtin",
-  },
-  Constant = {
-    "@constant.builtin",
-  },
-  Identifier = {
-    "@variable",
-    "@variable.builtin",
-  },
-  Function = {
-    "@function.builtin",
-    "@function.macro",
-    "@constructor",
-  },
-  Special = {
-    "@string.escape",
-  },
-  Todo = {
-    "@comment.todo",
-  },
-  PreProc = {
-    "@module",
-    "@module.builtin",
-  },
-}
+-- ui
+vim.api.nvim_set_hl(0, "StatusLine", { ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "StatusLineFileName", { ctermfg = "White" })
+vim.api.nvim_set_hl(0, "Pmenu", { ctermbg = "Black" })
+vim.api.nvim_set_hl(0, "PmenuSel", { reverse = true })
+vim.api.nvim_set_hl(0, "PmenuSbar", { ctermbg = "Black" })
+vim.api.nvim_set_hl(0, "PmenuThumb", { reverse = true })
+vim.api.nvim_set_hl(0, "LineNr", { ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { ctermfg = "White" })
+vim.api.nvim_set_hl(0, "Visual", { reverse = true })
+vim.api.nvim_set_hl(0, "WinSeparator", { ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "MatchParen", { reverse = true })
+vim.api.nvim_set_hl(0, "NonText", { ctermfg = "DarkGray" })
 
-for group_name, values in pairs(group_links) do
-  for _, v in ipairs(values) do
-    vim.api.nvim_set_hl(0, v, { link = group_name })
-  end
-end
-
-for col_name, values in pairs(syntax_colors) do
-  for _, v in ipairs(values) do
-    local value
-
-    value = { bg = "NONE", ctermbg = "NONE" }
-    value.ctermfg = col_name
-
-    vim.api.nvim_set_hl(0, v, value)
-  end
-end
-
-for group_name, values in pairs(ui_colors) do
-  local result = {}
-
-  for k, v in pairs(values) do
-    result[k] = v
-  end
-
-  vim.api.nvim_set_hl(0, group_name, result)
-end
-
-local group_styles = {
-  ["@type.builtin"] = { ctermfg = "Blue", italic = true },
-  ["@constant.builtin"] = { italic = true },
-  ["@variable.builtin"] = { italic = true },
-  ["@variable.parameter.builtin"] = { italic = true },
-  ["@function.builtin"] = { italic = true },
-  ["@attribute.builtin"] = { italic = true },
-  ["@module.builtin"] = { italic = true },
-  ["@tag.builtin"] = { italic = true },
-}
-
-for group_name, styles in pairs(group_styles) do
-  for style_name, style_value in pairs(styles) do
-    local hl = vim.api.nvim_get_hl(0, { name = group_name, create = false, link = false })
-    hl[style_name] = style_value
-
-    ---@diagnostic disable-next-line: param-type-mismatch
-    vim.api.nvim_set_hl(0, group_name, hl)
-  end
-end
-
-local diagnostic_signs = {
-  "DiagnosticSignError",
-  "DiagnosticSignWarn",
-  "DiagnosticSignInfo",
-  "DiagnosticSignHint",
-  "DiagnosticSignOk",
-}
-
-for _, sign in ipairs(diagnostic_signs) do
-  vim.fn.sign_define(sign, { text = "", numhl = sign })
-end
+-- diagnostics
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { sp = "Red", undercurl = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { sp = "Yellow", undercurl = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { sp = "Cyan", undercurl = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { sp = "Blue", undercurl = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineOk", { sp = "Green", undercurl = true })
+vim.fn.sign_define("DiagnosticSignError", { text = "", numhl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", numhl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", numhl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", numhl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignOk", { text = "", numhl = "DiagnosticSignOk" })
