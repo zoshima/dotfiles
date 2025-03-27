0255529b9cf1cb938fd83442dbfdfea2a5895256
zoshima <dmykras@gmail.com>
2025-03-27 10:32:23 +0100

    migrate to single file config

--- Makefile
+++ Makefile
@@ -1,15 +1,10 @@
+.PHONY: bash nvim
+
 bash:
 	rm ~/.bashrc || true
 	ln -s ~/git/dotfiles/.bashrc ~/.bashrc
 
-ghostty:
-	ln -s ~/git/dotfiles/.ghostty ~/.ghostty
-
 nvim:
-	rm -rf ~/.config/nvim || true
-	mkdir -p ~/.config/nvim/pack/git
-	ln -s ~/git/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
-	ln -s ~/git/dotfiles/nvim/lua ~/.config/nvim/lua
+	rm -rf ~/.config/nvim || true && mkdir -p ~/.config/nvim/pack/git
 	git clone https://github.com/nvim-treesitter/nvim-treesitter.git ~/.config/nvim/pack/git/start/nvim-treesitter
-
-.PHONY: bash nvim
+	ln -s ~/git/dotfiles/nvim.lua ~/.config/nvim/init.lua
---
+++ nvim.lua
@@ -0,0 +1,156 @@
+-- options
+vim.opt.termguicolors = false
+vim.opt.signcolumn = "number"
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.pumheight = 5
+vim.opt.foldenable = false
+vim.opt.swapfile = false
+vim.opt.shadafile = "NONE"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "nv"
+vim.opt.mousemodel = "extend"
+vim.opt.scrolloff = 1
+vim.opt.path:append("**/*")
+vim.opt.winborder = "rounded"
+vim.opt.completeopt = { "menuone", "noselect", "fuzzy" }
+vim.opt.listchars = { tab = "<>", space = "_", eol = "$" }
+vim.opt.fillchars = { msgsep = "─", stl = "─", stlnc = "─" }
+vim.opt.statusline = "%m%r[%f][%l:%c]%=%y"
+
+-- keymaps
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
+vim.keymap.set("n", "<Space>e", ":Ex<CR>")
+vim.keymap.set("n", "<Space>p", ":find ")
+vim.keymap.set("n", "<Space>f", ":silent grep! ")
+vim.keymap.set("n", "<Space>b", ":ls<CR>:b ")
+vim.keymap.set("n", "<Space>yp", function()
+  local file_path = vim.fn.expand('%:p')
+  vim.fn.setreg('+', file_path)
+  vim.notify("\"" .. file_path .. "\" yanked to clipboard", vim.log.levels.INFO)
+end)
+
+-- autocmd
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { "*.json", "*.css", "*.scss", "*.html", },
+  callback = function()
+    vim.cmd(":silent !npx prettier % --write")
+  end,
+})
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+    if client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
+vim.api.nvim_create_autocmd('DiagnosticChanged', {
+  callback = function()
+    vim.cmd("redrawstatus")
+  end
+})
+
+-- lsp
+vim.lsp.config["bashls"] = {
+  cmd = { "bash-language-server", "start" },
+  filetypes = { "sh" }
+}
+vim.lsp.config["gopls"] = {
+  cmd = { "gopls" },
+  root_markers = { "go.mod", "go.work", ".git" },
+  filetypes = { "go" }
+}
+vim.lsp.config["tsls"] = {
+  cmd = { "typescript-language-server", "--stdio" },
+  root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+  filetypes = { "typescript", "javascript" }
+}
+vim.lsp.config["luals"] = {
+  cmd = { "lua-language-server" },
+  root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+  filetypes = { "lua" },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+        }
+      }
+    }
+  }
+}
+
+vim.lsp.enable({ "bashls", "gopls", "tsls", "luals" })
+vim.diagnostic.config({ jump = { float = true } })
+
+-- colorscheme
+-- syntax (:h cterm-colors, :h group-name)
+vim.api.nvim_set_hl(0, "Comment", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "Constant", { ctermfg = "Green", });
+vim.api.nvim_set_hl(0, "Identifier", { ctermfg = "White", });
+vim.api.nvim_set_hl(0, "Function", { ctermfg = "Yellow", });
+vim.api.nvim_set_hl(0, "Statement", { ctermfg = "Red", });
+vim.api.nvim_set_hl(0, "Operator", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "PreProc", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "Type", { ctermfg = "Cyan", });
+vim.api.nvim_set_hl(0, "Special", { ctermfg = "Magenta", });
+vim.api.nvim_set_hl(0, "Tag", { ctermfg = "Blue", });
+vim.api.nvim_set_hl(0, "Delimiter", { ctermfg = "Gray", });
+vim.api.nvim_set_hl(0, "Underlined", { underline = true });
+vim.api.nvim_set_hl(0, "Ignore", { ctermfg = "DarkGray", });
+vim.api.nvim_set_hl(0, "Error", { ctermfg = "Red", });
+vim.api.nvim_set_hl(0, "Todo", { ctermfg = "Brown", });
+vim.api.nvim_set_hl(0, "Added", { ctermfg = "Green", });
+vim.api.nvim_set_hl(0, "Changed", { ctermfg = "Yellow", });
+vim.api.nvim_set_hl(0, "Removed", { ctermfg = "Red", });
+-- ui (:h highlight-groups)
+vim.api.nvim_set_hl(0, "Directory", { ctermfg = "Blue", bold = true })
+vim.api.nvim_set_hl(0, "StatusLine", { ctermbg = "NONE", ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { ctermbg = "NONE", ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "Pmenu", { ctermbg = "Black" })
+vim.api.nvim_set_hl(0, "PmenuSel", { reverse = true })
+vim.api.nvim_set_hl(0, "PmenuSbar", { ctermbg = "Black" })
+vim.api.nvim_set_hl(0, "PmenuThumb", { reverse = true })
+vim.api.nvim_set_hl(0, "LineNr", { ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { ctermfg = "White" })
+vim.api.nvim_set_hl(0, "Visual", { reverse = true })
+vim.api.nvim_set_hl(0, "WinSeparator", { ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "MatchParen", { bold = true })
+vim.api.nvim_set_hl(0, "NonText", { ctermfg = "DarkGray" })
+vim.api.nvim_set_hl(0, "FloatBorder", { ctermfg = "DarkGray" })
+-- diagnostics (:h diagnostic-highlights)
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { link = "SpellBad" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { link = "SpellCap" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { link = "SpellRare" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { link = "SpellRare" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineOk", { link = "SpellLocal" })
+-- treesitter
+vim.api.nvim_set_hl(0, "@module", { link = "PreProc" });
+vim.api.nvim_set_hl(0, "@constructor", { link = "Function" });
+vim.api.nvim_set_hl(0, "@type.builtin", { ctermfg = "Blue", italic = true });
+vim.api.nvim_set_hl(0, "@constant.builtin", { ctermfg = "Green", italic = true });
+vim.api.nvim_set_hl(0, "@function.builtin", { ctermfg = "Yellow", italic = true });
+vim.api.nvim_set_hl(0, "@variable.builtin", { ctermfg = "White", italic = true });
+
+-- plugins
+require("nvim-treesitter.configs").setup({ highlight = { enable = true } })
